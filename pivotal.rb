2841b205dee63c84c77c0624018a050ae0b1b3c9
Marcus Derencius <derencius@gmail.com>
2017-05-04 11:40:22 -0700

    pleasing rubocop

--- pivotal.rb
+++ pivotal.rb
@@ -1,39 +1,38 @@
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'bundler/setup'
 require 'dotenv'
 Dotenv.load
 
-require "table_print"
+require 'table_print'
 require 'virtus'
 require 'active_support/all'
 require 'tracker_api'
 
-
 def format_date(date)
-  zone = "Pacific Time (US & Canada)"
-  date.in_time_zone(zone).strftime("%m/%d/%Y")
+  zone = 'Pacific Time (US & Canada)'
+  date.in_time_zone(zone).strftime('%m/%d/%Y')
 end
 
-client = TrackerApi::Client.new(token: ENV["TRACKER_TOKEN"])
+client = TrackerApi::Client.new(token: ENV['TRACKER_TOKEN'])
 start_at = 35.days.ago
 
-activities = client.activity(occurred_after: start_at.iso8601, fields: ":default,message,changes,primary_resources,occurred_at,kind")
+activities = client.activity(occurred_after: start_at.iso8601, fields: ':default,message,changes,primary_resources,occurred_at,kind')
 
 parsed_activities = activities.map do |activity|
   result = {
     us_date: format_date(activity.occurred_at),
     raw_date: activity.occurred_at,
     kind: activity.kind,
-    msg: activity.message,
+    msg: activity.message
   }
   if story_id = activity.try(:primary_resources).try(:first).try(:id)
     story = client.story(story_id)
-    result.merge!({
-      story_id: story_id,
-      story_name: story.name
-    })
+    result[:story_id] = story_id
+    result[:story_name] = story.name
   else
-    result.merge!({ kind: "ERROR #{result[:kind]}"})
+    result[:kind] = "ERROR #{result[:kind]}"
   end
   result
 end
